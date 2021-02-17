93e16e20b22a1ee4827ab892a6051745bd43596c
Bruno Cerciliar <brunocerciliar@gmail.com>
2021-02-17 04:58:30 -0300

    Change sequelize to typeorm

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,18 +1,3 @@
-import { Sequelize } from 'sequelize-typescript';
-import { sequelizeOptions } from '@/config/database'
+import { createConnection } from 'typeorm'
 
-class Database {
-  public connection: Sequelize;
-
-  constructor() {
-    this.init();
-  }
-
-  init(): void {
-    this.connection = new Sequelize(sequelizeOptions);
-  }
-}
-
-const database: Database = new Database;
-
-export default database;
+createConnection()
